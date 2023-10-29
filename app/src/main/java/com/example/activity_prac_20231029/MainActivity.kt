036b5ee828cf04b06a9897a374fa598c6fcec569
mnyu123 <whdudwo5582@gmail.com>
2023-10-29 10:34:50 +0900

    숫자 전달받아서 출력 , 기본값이 설정되어야함.

--- app/src/main/java/com/example/activity_prac_20231029/MainActivity.kt
+++ app/src/main/java/com/example/activity_prac_20231029/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 //            화면 이동 -> Intent 클래스
 
 //            1번 어디서 출발해서 어디로 도착할지 정보 명시
-            var myIntent = Intent(this,SecondActivity::class.java)
+            var myIntent = Intent(this, SecondActivity::class.java)
 
 //            2번 startActivity로 이동
             startActivity(myIntent)
@@ -29,7 +29,20 @@ class MainActivity : AppCompatActivity() {
         binding.thridBtn.setOnClickListener {
 //            3번 화면으로 이동하는 내용
 //            문구 전달은 아직
-            val myIntent = Intent(this,ThirdActivity::class.java)
+            val myIntent = Intent(this, ThirdActivity::class.java)
+//            이동(start) 하기 전에 데이터를 myIntent에 첨부
+//            edtText1에 썼던 내용을 변수에 저장하고
+            val inputMsg = binding.edtText1.text.toString()
+//            그 변수를 첨부할것임.
+//            저 메시지는 inputMsg의 값에 있는 태그 , 딱지같은 거임
+            myIntent.putExtra("메시지", inputMsg)
+//            정상적으로 첨부는 되었으나 3번째 화면에서도 따로 보여주게 구현해야함.
+
+//            숫자를 inputNum에 대입
+            val inputNum = binding.edtText2.text.toString().toInt()
+
+            myIntent.putExtra("숫자",inputNum)
+
             startActivity(myIntent)
         }
     }
--- app/src/main/java/com/example/activity_prac_20231029/ThirdActivity.kt
+++ app/src/main/java/com/example/activity_prac_20231029/ThirdActivity.kt
@@ -2,6 +2,7 @@ package com.example.activity_prac_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.activity_prac_20231029.databinding.ActivityThirdBinding
 
@@ -10,5 +11,22 @@ class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_third)
+
+//        3번 화면이 "열린"(보인) 순간 1번 화면에서 보내준 String을 받아서 txtMsg에 반영
+//        메시지 태그 붙어있는 메시지를 get 하셈
+        val receiveMsg = intent.getStringExtra("메시지")
+
+
+//       1번 화면에서준 "숫자" 태그의 내용을 받음
+//        string처럼 보내면 "기본값" 이 없기 때문에 에러가남
+//        왜?  int는 자바의 기본형 변수라서 , "첨부가 안될때의" 기본값이 설정 되어야함.
+        val receiveNum = intent.getIntExtra("숫자", 0)
+
+        //        get한 메시지를 txtMsg에 .text형태로 대입해서 넣음
+        binding.txtMsg.text = "전달 받은 내용 : ${receiveMsg} \n전달 받은 숫자 : ${receiveNum}"
+
+        Toast.makeText(this, "받은 내용: ${receiveMsg}\n받은 숫자: ${receiveNum}", Toast.LENGTH_SHORT).show()
+
+
     }
 }
